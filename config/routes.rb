89631f29f9321d79b2f23ab5862397365aab1279
Yuta <yuta.shogai@gmail.com>
2021-02-09 11:33:14 +0000

    [Updated] 新規登録・ログイン機能実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "blogs#index"
   resources :blogs, only: [:show, :new, :create, :edit, :update, :destroy]
