889c426c573f0d69b8c48a111ebe962395c4484e
Yuta <yuta.shogai@gmail.com>
2021-02-12 16:31:02 +0000

    [Update] 検索機能実装完了

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get '/search' => 'search#search'
+  get 'searches/search' => 'searches#search', as: "search"
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "homes#top"
