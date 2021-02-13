44aaa4ac4a054aa8f88fbe1364b0d5788b194654
Yuta <yuta.shogai@gmail.com>
2021-02-13 11:25:55 +0000

    [Updated] いいね非同期　実装

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -4,14 +4,12 @@ class FavoritesController < ApplicationController
     @blog = Blog.find(params[:blog_id])
     @favorite = current_user.favorites.new(blog_id: @blog.id)
     @favorite.save
-    redirect_back(fallback_location: root_path)
   end
 
   def destroy
     @blog = Blog.find(params[:blog_id])
     @favorite = current_user.favorites.find_by(blog_id: @blog.id)
     @favorite.destroy
-    redirect_back(fallback_location: root_path)
   end
 
 end
