573c0306084e16103d1806048577723b7bf42fa4
Yuta <yuta.shogai@gmail.com>
2021-02-19 15:58:44 +0000

    [Updated] チャット機能 各モデルのアソシエーション記述

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,6 +11,9 @@ class User < ApplicationRecord
   has_many :passive_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
   has_many :following, through: :active_relationships, source: :followed
   has_many :followers, through: :passive_relationships, source: :follower
+  has_many :user_rooms, dependent: :destroy
+  has_many :chats, dependent: :destroy
+  has_many :rooms, through: :user_rooms, dependent: :destroy
   attachment :profile_image
 
   def follow(other_user)
