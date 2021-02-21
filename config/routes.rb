45d0f75024a906b5b7d8b7193e6ad6ada0be69b2
Yuta <yuta.shogai@gmail.com>
2021-02-21 15:31:44 +0000

    [Updated] ルーティング、chatsコントローラーへ記述

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,22 @@
 Rails.application.routes.draw do
-  get 'chats/show'
   get 'searches/search' => 'searches#search', as: "search"
   devise_for :users#, controllers: {registrations: 'users/registrations'}
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "homes#top"
+
   resources :blogs do
     resources :blogcomments, only: [:create, :destroy]
     resource :favorites, only: [:create, :destroy]
   end
+
   resources :users, only: [:show, :index, :edit, :update] do
     member do
       get :following, :followers
     end
   end
+
   resources :relationships, only: [:create, :destroy]
+
+  get 'chat/:id' => 'chats#show', as: 'chat'
+  resources :chats, only: [:create]
 end
